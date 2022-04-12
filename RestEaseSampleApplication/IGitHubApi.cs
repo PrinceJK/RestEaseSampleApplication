ad99a95cc42857e97e83fe359f3b0831fc1bd8e0
John <jkehinde@axxess.com>
2022-04-12 16:30:56 +0100

    Add project files.

---
+++ RestEaseSampleApplication/IGitHubApi.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using RestEase;
+
+namespace RestEaseSampleApplication
+{
+    [Header("User-Agent", "RestEase")]
+    public interface IGitHubApi
+    {
+        [Get("users/{userId}")]
+        Task<User> GetUserAsync([Path] string userId);
+    }
+}
